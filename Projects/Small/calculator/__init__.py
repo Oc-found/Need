8961fcff991f5f43848ff0286bc50b4d7144cef6
BwYEa <153354935+BwYEa@users.noreply.github.com>
2024-02-17 13:25:11 +0800

    Update __init__.py

--- Projects/Small/calculator/__init__.py
+++ Projects/Small/calculator/__init__.py
@@ -1,2 +1,16 @@
 from ver import ver
+from run import run
 print(f"Calculator v{ver}")
+
+while 1:
+  #If not int()
+  try:
+    a = int(input("First Number: "))
+    b = int(input("Second Number: "))
+  except:
+    pass
+  c = input("+ - * / : ")
+  #########
+  d = run(a,b)
+  print(d.runner())
+  
